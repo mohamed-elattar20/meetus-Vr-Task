ca4f29203bc80b0bee441f21cb218453ec32036c
Mohamed Elattar <mohamedelattarm819@gmail.com>
2024-02-11 19:18:20 +0200

    Add project Structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Class Component</h1>
+      </>
+    );
+  }
+}
